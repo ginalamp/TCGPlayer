68a1465582146d115d7cf648a1ccad1ceb12961d
ginalamp <gina.n.lamprecht@gmail.com>
2021-10-18 10:42:39 +0200

    Read in bulk data as pandas dataframe

--- src/tcgplaya/utils.py
+++ src/tcgplaya/utils.py
@@ -1,6 +1,8 @@
 # Dealing with the api/data
 import requests
 import json
+import pandas as pd
+from tcgplaya.models import *
 
 # print out hardcoded request to the search API
 def api_basic_call():
@@ -14,3 +16,10 @@ def jprint(obj):
    # create a formatted string of the Python JSON object
    text = json.dumps(obj, sort_keys=True, indent=4)
    print(text)
+
+def parse_bulk_data(filename):
+    with open(filename, 'r') as f:
+        bulkdata = json.load(f)
+
+    df = pd.DataFrame(bulkdata)
+    print(df)
