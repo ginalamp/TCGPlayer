921f35e2c4b7cc03adca99ac2084ce979ce3d93f
ginalamp <gina.n.lamprecht@gmail.com>
2021-10-19 12:01:24 +0200

    Update card url

--- src/project/urls.py
+++ src/project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    path('cardlisting/', include('tcgplaya.urls')),
+    path('cards/', include('tcgplaya.urls')),
 ]
