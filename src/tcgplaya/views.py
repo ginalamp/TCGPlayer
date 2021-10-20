ebbb833020200f65c222b59d524958160603d408
ginalamp <gina.n.lamprecht@gmail.com>
2021-10-21 01:51:55 +0200

    Attempt to create new cardlistings, but not working properly
    Struggling with profile field

--- src/tcgplaya/views.py
+++ src/tcgplaya/views.py
@@ -42,13 +42,25 @@ def cardlisting_view(request, id):
 
     return render(request, 'tcgplaya/cardlisting.html', context)
 
+# create a new cardlisting from a given card
 def new_cardlisting_view(request, id):
     context = {}
     print(request)
     if request.method == "POST":
         listing_price = request.POST.get('listing_price')
         print("listing price --->", listing_price)
+        # create new cardlisting
         card = Card.objects.get(id=id)
+        print(request.user)
+        # print(request.user.profile)
+        if listing_price is not None:
+            cardlisting = CardListing.objects.create(
+                card=card,
+                seller=request.user,
+                price=listing_price
+            )
+            print("new cardlisting created: ", cardlisting)
+
         context = {
             'card': card,
             'suggest_price': card.usd,
