f77d7df88f5f65e46514a5df1fb3f241c450bfe0
ginalamp <gina.n.lamprecht@gmail.com>
2021-10-20 15:49:07 +0200

    Remove old home page

--- src/tcgplaya/urls.py
+++ src/tcgplaya/urls.py
@@ -4,7 +4,6 @@ from . import views
 # tcgplaya url configuration
 app_name = 'tcgplaya'
 urlpatterns = [
-    path('home/', views.home_view),
     path('card/<int:id>', views.card_view),
     path('cardlistings/', views.cardlistings_view),
     path('cardlisting/<int:id>', views.cardlisting_view),
