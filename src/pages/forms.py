82846ccf11724c7473120c96fd64839b87a30e33
Emma Raaff <elraaff@gmail.com>
2021-10-20 11:02:43 +0200

    Login and register form functional - needs some styling stuff still

--- src/pages/forms.py
+++ src/pages/forms.py
@@ -5,14 +5,11 @@ from django.core.validators import RegexValidator
 
 
 class CreateUserForm(UserCreationForm):
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.") # to check phone number entered by user
-    phone = forms.CharField(validators=[phone_regex], max_length=17)
+    # phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.") # to check phone number entered by user
+    # phone = forms.CharField(validators=[phone_regex], max_length=17)
     class Meta:
         model = User
         fields = [
             'username',
             'email',
-            'password1',
-            'password2',
-            'phone'
         ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -1,7 +1,11 @@
 # Renders the general html templates
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
 
 # Import user creation form that I customised from the django standard UserCreationForm
 # Customised to add an email field and phone number field - since a seller must provide contact info
@@ -13,29 +17,45 @@ def home_view(request, *args, **kwargs):
     return render(request, 'home.html', context)
 
 # register page
-def register_view(request, *args, **kwargs):
+def register_view(request):
     form = CreateUserForm()
     
     if request.method == 'POST':
-        form = CreateUserForm(request.POST)
+        form = CreateUserForm(request.POST or None)
         if form.is_valid():
             form.save()
-            return redirect('login_view')
+            form = CreateUserForm() # makes form blank after saving
+            return redirect("/login/")
     
     context = {'form':form}
     return render(request, 'register.html', context)
 
 # login page
-def login_view(request, *args, **kwargs):
-    context = {}
-    return render(request, 'login.html', context)
+def login_view(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    print(username)
+    print(password)
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        # login(request, user)
+        # Redirect to a success page.
+        return redirect('/')
+    else:
+        # Return san 'invalid login' error message.
+        print('who u')
+    
+    # profile = user.profile
+    # context = {}
+    # return render(request, 'login.html', context)
+    return render(request, 'login.html', {})
 
 # cart page
-def cart_view(request, *args, **kwargs):
+def cart_view(request):
     context = {}
     return render(request, 'cart.html', context)
 
 # profile
-def profile_view(request, *args, **kwargs):
+def profile_view(request):
     context = {}
     return render(request, 'profile.html', context)
